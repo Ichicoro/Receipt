33bc3fafb84d028aad6e6eda26cb9e029eccadba
Zelda <ze@zelda.sh>
2025-07-15 14:48:10 +0200

    Initial Commit

---
+++ Receipt/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Receipt
+//
+//  Created by Zelda on 15/07/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
